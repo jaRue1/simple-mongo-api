0c48bcc96f1b48faf1b40f31b12d81daf37847d1
jarue1 <jaruejohnson@outlook.com>
2023-05-06 14:27:23 -0400

    dtt

--- src/controllers/myControllers.js
+++ src/controllers/myControllers.js
@@ -49,3 +49,56 @@ exports.createProduct = (req, res) => {
     })
     .catch((err) => res.status(400).json("Error: " + err))
 }
+// update products
+exports.updateProduct = (req, res) => {
+  Product.findById(req.params.id)
+    .then((product) => {
+      product.name = req.body.name
+      product.price = req.body.price
+      product.description = req.body.description
+
+      product
+        .save()
+        .then(() => res.json("Product updated!"))
+        .catch((err) => res.status(400).json("Error: " + err))
+    })
+    .catch((err) => res.status(400).json("Error: " + err))
+}
+// delete products
+exports.deleteProduct = (req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Product deleted."))
+    .catch((err) => res.status(400).json("Error: " + err))
+}
+// get single product
+exports.getProduct = (req, res) => {
+  Product.findById(req.params.id)
+    .then((product) => res.json(product))
+    .catch((err) => res.status(400).json("Error: " + err))
+}
+// get single user
+exports.getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json("Error: " + err))
+}
+// update users
+exports.updateUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      user.username = req.body.username
+      user.password = req.body.password
+
+      user
+        .save()
+        .then(() => res.json("User updated!"))
+        .catch((err) => res.status(400).json("Error: " + err))
+    })
+    .catch((err) => res.status(400).json("Error: " + err))
+}
+// delete users
+exports.deleteUser = (req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json("User deleted."))
+    .catch((err) => res.status(400).json("Error: " + err))
+}
--- src/routes/myRoutes.js
+++ src/routes/myRoutes.js
@@ -5,10 +5,22 @@ const myController = require("../controllers/myControllers")
 
 router.get("/users", myController.getUsers)
 
+router.get("/users/:id", myController.getUserById)
+
 router.post("/users", myController.createUser)
 
 router.get("/products", myController.getProducts)
 
+router.get("/products/:id", myController.getProductById)
+
 router.post("/products", myController.createProduct)
+
+router.put("/products/:id", myController.updateProduct)
+
+router.put("/users/:id", myController.updateUser)
+
+router.delete("/products/:id", myController.deleteProduct)
+
+router.delete("/users/:id", myController.deleteUser)
 
 module.exports = router
